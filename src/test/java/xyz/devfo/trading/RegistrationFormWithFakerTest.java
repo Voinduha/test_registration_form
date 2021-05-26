1dceee9e8beacce48a888e62480231024b73c877
DanVu <danvu@ya.ru>
2021-05-26 17:54:57 +0300

    added test with java faker

--- src/test/java/xyz/devfo/trading/RegistrationFormWithFakerTest.java
+++ src/test/java/xyz/devfo/trading/RegistrationFormWithFakerTest.java
@@ -9,11 +9,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormWithFakerTest {
 
-    @BeforeAll
-    static void setup() {
-        Configuration.startMaximized = true;
-    }
-
     Faker faker = new Faker();
 
     String firstname = faker.name().firstName(),
@@ -21,6 +16,11 @@ public class RegistrationFormWithFakerTest {
            email = faker.internet().emailAddress(),
            phone = faker.number().digits(10),
            password = faker.internet().password(9, 15);
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
 
     @Test
     void successRegistrationForm() {
